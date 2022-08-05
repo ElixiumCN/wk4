b730aae9b5802dc52097c96a9fcb0459ae870201
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-08-05 04:53:45 +0100

    .
    .

--- cyberpet.js
+++ cyberpet.js
@@ -1,6 +1,7 @@
 // todo: doing now: classes & subclasses
 import inquirer from 'inquirer';
 
+
 // Variables
 let petHappy = 100;
 let petClean = 100;
@@ -11,6 +12,40 @@ let petHunger = 100;
 // 2 = likes a lot[add 20 points]
 // 3 = no stats modified
 
+
+function addHappyPoints() {
+    if(petHappy<=99&&petHappy!=0){
+        petHappy += 20;
+        console.log(`${response.getName} is 20 points happier.`);
+    }
+    else if(petHappy==0){
+        petHappy+=0;
+        console.log(`${response.getName} can't be happier.`);
+    }
+}
+
+function addCleanPoints() {
+    if(petClean<=99&&petClean!=0){
+        petClean += 20;
+        console.log(`${response.getName} is 20 points cleaner.`);
+    }
+    else if(petClean==0){
+        petClean+=0;
+        console.log(`${response.getName} is already completely clean.`);
+    }
+}
+
+function addHungerPoints() {
+    if(petHunger<=99&&petHunger!=0){
+        petHunger += 20;
+        console.log(`${response.getName} is 20 points less hungry.`);
+    }
+    else if(petHunger==0){
+        petHunger+=0;
+        console.log(`${response.getName} can't eat anymore.`);
+    }
+}
+
 // main Class
 class cyberPet {
     constructor (name) {
@@ -18,12 +53,52 @@ class cyberPet {
     this.happy = 100
     this.clean = 100
     this.hunger = 100
+
+
+    //
 }
 }
+
+// const myFunctions = require('subHamster.cjs');
+// import Hamster from 'subHamster.js';
+
 
 // Subclass
 class Hamster extends cyberPet {
     constructor(name) {
+        super (name)
+        this.name = name
+        this.eatLettuce = 0
+        this.eatMeat = 0
+        this.eatDry = 1
+        this.playBone = 0
+        this.playBall = 0
+        this.playWool = 3
+        this.playCleans = 0
+        this.cleanPoints = 1
+        this.cleanHappyPoints = 1
+}   
+}
+
+class Cat extends cyberPet {
+    constructor(name) {
+        super (name)
+        this.name = name
+        this.eatLettuce = 0
+        this.eatMeat = 0
+        this.eatDry = 1
+        this.playBone = 0
+        this.playBall = 0
+        this.playWool = 3
+        this.playCleans = 0
+        this.cleanPoints = 1
+        this.cleanHappyPoints = 1
+}   
+}
+
+class Dog extends cyberPet {
+    constructor(name) {
+        super (name)
         this.name = name
         this.eatLettuce = 0
         this.eatMeat = 0
@@ -57,8 +132,17 @@ const response = await inquirer.prompt(questions)
 // let playersPet = new Hamster(response.getName)
 // creating new object of cyberPet class with the name response of the questions.
 
-let playerPetChoice = `${response.getType}` // create var holding the player chosen subclass (animal type)
-const theCyberPet = new playerPetChoice; // spawns the player chosen subclass (animal type)
+let pet 
+if(response.getType === 'Hamster'){
+    pet = new Hamster(response.getType)
+} else if(response.getType === 'Dog'){
+    pet = new Dog(response.getType)
+} else if(response.getType === 'Cat'){
+    pet = new Cat(response.getType)
+}
+
+// let playerPetChoice = `${response.getType}` // create var holding the player chosen subclass (animal type)
+// const theCyberPet = new playerPetChoice; // spawns the player chosen subclass (animal type)
 console.log(`You have chosen ${response.getType}`);
 console.log(`You have named your pet ${response.getName}`);
 
@@ -116,38 +200,7 @@ function gameovercheck(){
     }
 }
 
-function addHappyPoints() {
-    if(petHappy<=99&&petHappy!=0){
-        petHappy += 20;
-        console.log(`${response.getName} is 20 points happier.`);
-    }
-    else if(petHappy==0){
-        petHappy+=0;
-        console.log(`${response.getName} can't be happier.`);
-    }
-}
 
-function addCleanPoints() {
-    if(petClean<=99&&petClean!=0){
-        petClean += 20;
-        console.log(`${response.getName} is 20 points cleaner.`);
-    }
-    else if(petClean==0){
-        petClean+=0;
-        console.log(`${response.getName} is already completely clean.`);
-    }
-}
-
-function addHungerPoints() {
-    if(petHunger<=99&&petHunger!=0){
-        petHunger += 20;
-        console.log(`${response.getName} is 20 points less hungry.`);
-    }
-    else if(petHunger==0){
-        petHunger+=0;
-        console.log(`${response.getName} can't eat anymore.`);
-    }
-}
 
 function printRealtimeStats(){
     process.stdout.clearLine();
@@ -165,25 +218,47 @@ const questions2 = [
     
 ]
 
-inquirer
-.prompt([
-{
-    type:'list',
-    name: 'getAction',
-    message: "What's your chosen action?",
-    choices: ['Play', 'Feed', 'Clean'],
+// inquirer
+// .prompt([
+// {
+//     type:'list',
+//     name: 'getAction',
+//     message: "What's your chosen action?",
+//     choices: ['Play', 'Feed', 'Clean'],
 
-},
-])
-.then((answers) => {
-    console.log(`Your chosen action is ${answers.getAction}`);
-    if(`${answers.getAction}` === 'Feed') {
+// },
+// ])
+// .then((answers) => {
+//     console.log(`Your chosen action is ${answers.getAction}`);
+//     if(`${answers.getAction}` === 'Feed') {
+//         addHungerPoints();
+//     } 
+//     else if(`${answers.getAction}` === 'Clean') {
+//         addCleanPoints();    }   
+//     else if(`${answers.getAction}` === 'Play') {
+//         addHappyPoints();
+//     }   
+// });
+
+const actions = async () => {
+    const doing = await inquirer.prompt([
+        {
+            type:'list',
+            name: 'getAction',
+            message: "What's your chosen action?",
+            choices: ['Play', 'Feed', 'Clean', new inquirer.Separator()],  
+        }
+    ])
+    .then((answers) => {
+    if(answers.getAction === 'Feed') {
         addHungerPoints();
     } 
-    else if(`${answers.getAction}` === 'Clean') {
+    else if(answers.getAction === 'Clean') {
         addCleanPoints();    }   
-    else if(`${answers.getAction}` === 'Play') {
+    else if(answers.getAction === 'Play') {
         addHappyPoints();
-    }   
-});
-
+    } 
+    actions()
+})
+}  
+actions() 
